b3e611cb4da8507d5f1ffe77d02696f4c1281c0e
Palle Raabjerg <github@raabjerg.mm.st>
2014-07-25 19:40:30 +0200

    Added Maltron keymap

--- js/keymaps.js
+++ js/keymaps.js
@@ -25,6 +25,34 @@ var KEYMAPS =
 
          'singleshot'    : [113],        //f2
          'thumbshots'    : [114],        //f3
+         'resizethumbs'  : [115]         //f4
+     },
+     'maltron':
+     {
+         'next'          : [32, 34, 39], //space, pgdn, right
+         'prev'          : [8, 33, 37],  //backspace, pgup, left
+         'toggleswitcher': [27, 116],    //esc, f5
+         //Event editor
+         'canceledit'    : [27],         //esc
+         'toggleedit'    : [9],          //tab
+         'clearevent'    : [8],          //(shift) + backspace
+         'slideleft'     : [78],         //n
+         'slideright'    : [83],         //s
+         'selectslide'   : [13],         //enter
+         'eventdown'     : [73],         //i (w. alt for move event down, w. shift for add event)
+         'eventup'       : [89],         //y (w. alt for move event up)
+
+         'rotate'        : [82],         //r
+         'grab'          : [71],         //g
+         'fade'          : [70],         //f
+         'scale'         : [67],         //c
+         'rotateY'       : [76],         //l
+         'rotateX'       : [88],         //x
+         'sizew'         : [87],         //w
+         'sizeh'         : [72],         //h
+
+         'singleshot'    : [113],        //f2
+         'thumbshots'    : [114],        //f3
          'resizethumbs'  : [115],        //f4
      }
     }
