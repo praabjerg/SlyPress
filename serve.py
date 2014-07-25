8fb366f5bcfc175a2a7e6568ac33bfba96f2815e
Arild Haugstad <ammh@haugstad-terkelsen.dk>
2014-07-25 19:39:01 +0200

    general Python cleanup serve.py   - using cherrypy.expose decorator   - inheriting from object   - using string format()   - opening files with context processors   - more blank lines   - pep8 formatting

--- serve.py
+++ serve.py
@@ -31,52 +31,67 @@ from mathtex import compile_elt
 
 jenc = json.JSONEncoder()
 
-class BackupSys:
+
+class BackupSys(object):
+    numfilename = 'backup/num'
+
     def __init__(self):
         self.num = 0
-        if os.path.exists('backup/num'):
-            numfileread = open('backup/num')
-            self.num = int(numfileread.readline())
-            numfileread.close()
+        if os.path.exists(self.numfilename):
+            with open(self.numfilename) as f:
+                self.num = int(f.readline())
+
     def increment(self):
         self.num += 1
-        numfilewrite = open('backup/num', 'w')
-        numfilewrite.write(str(self.num))
-        numfilewrite.close()
+        with open(self.numfilename, 'w') as f:
+            f.write(str(self.num))
+
     def get_num(self):
         return self.num
 
-class Bleamer:
+
+class Bleamer(object):
     def __init__(self):
         self.backupsys = BackupSys()
+
         def loadpage(driver):
             sleep(2)
             driver.get('http://localhost:8080/present.html')
+
         chromedriver = './chromedriver'
         os.environ['webdriver.chrome.driver'] = chromedriver
         print("Loading Chromium!")
         self.webdriver = webdriver.Chrome(chromedriver)
         pload = Process(target=loadpage, args=(self.webdriver,))
         pload.start()
-        #self.webdriver.get('http://localhost:8080')
+        # self.webdriver.get('http://localhost:8080')
+
+    @cherrypy.expose
     def save_screenshot(self, filename, nw, se):
         self.webdriver.get_screenshot_as_file(filename)
         nw = json.loads(nw)
         se = json.loads(se)
-        print("Saving screenshot - " + "nw: " + str(nw) + ", se: " + str(se) + ", filename: " + filename + "\n")
+        print("Saving screenshot - nw: {}, se: {}, filename: {} ".format(
+            nw, se, filename))
         uncropped = misc.imread(filename)
-        range_y = [nw[1], se[1]]
-        range_x = [nw[0], se[0]]
-        cropped = uncropped[range_y[0]:range_y[1], range_x[0]:range_x[1]]
+        min_x = nw[0]
+        max_x = se[0]
+        min_y = nw[1]
+        max_y = se[1]
+        cropped = uncropped[min_y:max_y, min_x:max_x]
         misc.imsave(filename, cropped)
+
+    @cherrypy.expose
     def save_screenshot_scrot(self, filename, left):
         left = json.loads(left)
-        print("Saving screenshot with scrot - " + "filename: " + filename + "\n")
+        print("Saving screenshot with scrot - filename: {}".format(filename))
         os.system('scrot ' + filename)
         if left:
             os.system('mogrify -crop 1280x800+0+1142 ' + filename)
         else:
             os.system('mogrify -crop 1280x800+632+1142 ' + filename)
+
+    @cherrypy.expose
     def resize_thumbnails(self):
         imglist = os.listdir('thumb')
         for imagename in imglist:
@@ -85,48 +100,50 @@ class Bleamer:
                 image = misc.imread(filename)
                 smallimg = misc.imresize(image, (200, 267), 'bicubic')
                 misc.imsave('thumb/small' + filename[11:], smallimg)
+
+    @cherrypy.expose
     def get_image_size(self, imgname):
         image = misc.imread(imgname)
-        #ydim, xdim, weird = image.shape
-        return json.dumps([image.shape[1], image.shape[0]]);
+        # ydim, xdim, weird = image.shape
+        return json.dumps([image.shape[1], image.shape[0]])
+
+    @cherrypy.expose
     def pytutortrace(self, pyfile):
-        return generate_trace.get_json_trace(pyfile);
+        return generate_trace.get_json_trace(pyfile)
+
+    @cherrypy.expose
     def incrementbackup(self):
         self.backupsys.increment()
+
+    @cherrypy.expose
     def mathtex(self, name, formula, split):
         force = False
         if (not os.path.isfile('math_imgs/' + name + '.png')) or force:
             shutil.rmtree('math_imgs/' + name, True)
             compile_elt(name, formula, split)
+
+    @cherrypy.expose
     def outanims(self, animdata):
-        #cherrypy.response.headers['Content-Type'] = 'application/json'
-        f = open('working.json', 'w')
-        backup = open('backup/working' + str(self.backupsys.get_num()) + '.json', 'w')
-        f.write(animdata)
-        backup.write(animdata)
-        f.close()
-        backup.close()
+        # cherrypy.response.headers['Content-Type'] = 'application/json'
+        working_name = 'working.json'
+        backup_name = 'backup/working{}.json'.format(self.backupsys.get_num())
+        with open(working_name, 'w') as working, \
+                open(backup_name, 'w') as backup:
+            working.write(animdata)
+            backup.write(animdata)
         return json.dumps('Success!')
+
+    @cherrypy.expose
     def outxml(self, xmlstring):
         outstring = xmlstring.encode('utf-8')
-        #cherrypy.response.headers['Content-Type'] = 'application/json'
-        f = open('working.xml', 'w')
-        backup = open('backup/working' + str(self.backupsys.get_num()) + '.xml', 'w')
-        f.write(outstring)
-        backup.write(outstring)
-        f.close()
-        backup.close()
+        # cherrypy.response.headers['Content-Type'] = 'application/json'
+        working_name = 'working.xml'
+        backup_name = 'backup/working{}.xml'.format(self.backupsys.get_num())
+        with open(working_name, 'w') as working, \
+                open(backup_name, 'w') as backup:
+            working.write(outstring)
+            backup.write(outstring)
         return json.dumps('Success!')
-    incrementbackup.exposed = True
-    pytutortrace.exposed = True
-    outanims.exposed = True
-    outxml.exposed = True
-    save_screenshot.exposed = True
-    save_screenshot_scrot.exposed = True
-    resize_thumbnails.exposed = True
-    get_image_size.exposed = True
-    mathtex.exposed = True
-    #present.exposed = True
 
 
 config = {
@@ -136,12 +153,10 @@ config = {
     },
 }
 
+
 ROOT = os.path.abspath(os.path.dirname(__file__))
 
-config['/'] = {
-    'tools.staticdir.root': ROOT,
-    'tools.trailing_slash.on': False,
-}
+
 FILES = (
     'presentpre.html',
     'present.html',
@@ -151,6 +166,8 @@ FILES = (
     'original.json',
     'bleamer.dtd',
 )
+
+
 DIRS = (
     'css',
     'js',
@@ -162,11 +179,20 @@ DIRS = (
     'thumb',
 )
 
+
+config['/'] = {
+    'tools.staticdir.root': ROOT,
+    'tools.trailing_slash.on': False,
+}
+
+
 for path in FILES:
     config['/' + path] = {
         'tools.staticfile.on': True,
         'tools.staticfile.filename': os.path.join(ROOT, path),
     }
+
+
 for path in DIRS:
     config['/' + path] = {
         'tools.staticdir.on': True,
@@ -174,5 +200,5 @@ for path in DIRS:
     }
 
 
-#cherrypy.quickstart(root=Bleamer(), config="serve.conf")
+# cherrypy.quickstart(root=Bleamer(), config="serve.conf")
 cherrypy.quickstart(root=Bleamer(), config=config)
